d4f5f05538999eb6e4e76f09e4697b3399aa9a5c
FrankBonanno <fb0023@mix.wvu.edu>
2025-05-25 03:18:01 -0400

    ai voice integration

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CompanionComponent from '@/components/CompanionComponent';
 import { getCompanion } from '@/lib/actions/companion.actions';
 import { getSubjectColor } from '@/lib/utils';
 import { currentUser } from '@clerk/nextjs/server';
@@ -10,11 +11,13 @@ interface CompanionSessionPageProps {
 
 const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
 	const { id } = await params;
-	const { name, subject, title, topic, duration } = await getCompanion(id);
+	const companion = await getCompanion(id);
 	const user = await currentUser();
 
+	const { name, subject, topic, duration } = companion;
+
 	if (!user) redirect('/sign-in');
-	if (!name) redirect('/companions');
+	if (!companion) redirect('/companions');
 
 	return (
 		<main>
@@ -38,6 +41,8 @@ const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
 
 				<div className="items-start text-2xl max-md:hidden">{duration} minutes</div>
 			</article>
+
+			<CompanionComponent {...companion} companionId={id} userName={user.firstName} userImage={user.imageUrl} />
 		</main>
 	);
 };
